bef25903232557fbd73b66ac46dd4ff87196dfd7
MAALONSO <miguel.coba@gnp.com.mx>
2020-03-08 12:30:31 -0600

    Eliminando la dependencia de HDB2 y agregado una configuraciń de exclución a la clase principal de la aplicación

--- src/main/java/com/formacionbdi/springboot/app/commons/SpringbootServicioCommonsApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/commons/SpringbootServicioCommonsApplication.java
@@ -1,8 +1,16 @@
 package com.formacionbdi.springboot.app.commons;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+/**
+ * @EnableAutoconfiguration - Se desabilita el autoconfiguration del DataSource.
+ * @author ACER 141119
+ *
+ */
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class SpringbootServicioCommonsApplication {
 
 
